f64323a7e1f373729c31494f5994620f2e77ea23
Liepinsh <edgars.liepajnieks@gmail.com>
2018-09-22 16:31:42 +0300

    ruby 2.lesson 22.09.2018

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,6 @@
 class Player < ApplicationRecord
     belongs_to :team
+    validates :name, format: { with: /\A[a-zA-Z]+\z/, message: "must consist of letters only"}, presence: true
+    validates :number, uniqueness: true, length: {maximum: 2}, numericality: { only_integer: true, :greater_than_or_equal_to => 0}, presence: true
+    validates :position, presence: true, uniqueness: true, length: {maximum: 2}
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,7 @@
 class Team < ApplicationRecord
+    validates :title, format: { with: /\A[a-zA-Z]+\z/, message: "must consist of letters only"}, presence: true
+    validates :code, format: { with: /\A[a-zA-Z]+\z/, message: "must consist of letters only"}, presence: true
+
     def full_name
         "#{code} : #{title}"
     end
