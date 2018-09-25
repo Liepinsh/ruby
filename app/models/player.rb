4b539b8a36147b0e77be2a4404f568828eba91d0
Liepinsh <edgars.liepajnieks@gmail.com>
2018-09-25 13:05:37 +0300

    ruby 2.lesson 4.commit 25.09.2018

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,24 @@
 class Player < ApplicationRecord
     belongs_to :team
     validates :name, format: { with: /\A[a-zA-Z]+\z/, message: "must consist of letters only"}, presence: true
-    validates :number, uniqueness: true, length: {maximum: 2}, numericality: { only_integer: true, :greater_than_or_equal_to => 0}, presence: true
+    validates :number, length: {maximum: 2}, numericality: { only_integer: true, message: "is empty"}
+    validate :numbers
     validates :position, presence: true, uniqueness: true, length: {maximum: 2}
+    validates :club, format: { with: /\A[a-zA-Z]+\z/, message: "must consist of letters only"}, presence: true
+
+    def numbers
+        if number.blank? == false && number < 0
+            errors.add(:number, "must be greater than or equal to 0")
+        end
+    end
+
+
+    # validate :cls
+    # def cls
+    #     if club.blank? == true
+    #         errors.add(:club, "empty")
+    #     elsif club.with == /\A[a-zA-Z]+\z/
+    #         errors.add(:club, "other")
+    #     end
+    # end
 end
--- app/models/score.rb
+++ app/models/score.rb
@@ -2,8 +2,14 @@ class Score < ApplicationRecord
     belongs_to :home_team, class_name: 'Team'
     belongs_to :guest_team, class_name: 'Team'
     validates :home_score, numericality: { only_integer: true, :greater_than_or_equal_to => 0}, presence: true
-    # validates :home_score, numericality: { :even => @guest_score, message: "can't be the same as guest score" }
     validates :guest_score, numericality: { only_integer: true, :greater_than_or_equal_to => 0}, presence: true
+    validate :equal
+
+    def equal
+        if home_score == guest_score
+            errors.add(:home_score, "and Guest score can't be the same")
+        end
+    end
 
     def points_for_winning_team
         if winner_team == nil
